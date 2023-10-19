01d5b01342a060e422e3fbf198aa58eb15718599
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-19 11:35:40 +0100

    update password generator

--- password/password_generator.py
+++ password/password_generator.py
@@ -6,8 +6,36 @@ def generate_password(min_length, numbers=True, special_characters=True):
     digits = string.digits
     special = string.punctuation
     
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+        
+    pwd = ""
+    meets_criteria = False
+    has_numbers = False
+    has_special = False
     
-    print(letters, digits, special)
+    while not meets_criteria or len(pwd) < min_length:
+        new_char =  random.choice(characters)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_numbers = True
+        elif new_char in special:
+            has_special = True
+            
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_numbers
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+    return pwd
     
-    
-generate_password(10)
+   
+min_length = int(input("Enter the minimum length for your password: "))    
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"   
+has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"     
+pwd = generate_password(min_length, has_number, has_special )
+print("The generated password is:",pwd)
