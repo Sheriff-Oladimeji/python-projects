01663741bd77c09e2cc518803a0ce049e246817d
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-12-07 12:05:53 +0100

    feat: add new project

---
+++ weather/weather.py
@@ -0,0 +1,8 @@
+import requests
+
+API_KEY = "f7ae66582c022363444e1631a90c4455"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
